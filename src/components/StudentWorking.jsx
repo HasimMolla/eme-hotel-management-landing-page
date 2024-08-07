081924980e4d39fef6c5e0260b49616634b25ddb
Molla1441 <mhasim1441@gmail.com>
2024-08-07 11:55:30 +0530

    feat: News Channel and student working in section add Marquee

--- src/components/StudentWorking.jsx
+++ src/components/StudentWorking.jsx
@@ -5,10 +5,11 @@ import hyatt from '../../public/assets/images/hyatt.png'
 import itc from '../../public/assets/images/itc.png'
 import indigo from '../../public/assets/images/indigo.png'
 import Image from 'next/image'
+import Marquee from 'react-fast-marquee'
 function StudentWorking() {
     return (
         <div className="w-full  max-w-[1380px] mx-auto flex items-center justify-between  gap-[40px] py-5 md:py-8  px-[20px] md:px-24 ">
-            <div className="  text-[#042A76] lg:leading-5 lg:text-xl  font-bold">
+            <div className="w-[20%]  text-[#042A76] lg:leading-5 lg:text-xl  font-bold">
                 <div>
                     Our Students are
                 </div>
@@ -16,39 +17,45 @@ function StudentWorking() {
                     <span className="text-[#E87D1A]">W</span>orking In
                 </div>
             </div>
-
-            <div className='flex items-center justify-center gap-x-8'>
-                <Image
-                    width={80}
-                    height={80}
-                    src={taj}
-                alt='taj'/>
-                <Image
-                    width={120}
-                    height={120}
-                    src={westin}
-                alt='taj'/>
-                <Image
-                    width={120}
-                    height={120}
-                    src={hyatt}
-                alt='taj'/>
-                <Image
-                    width={120}
-                    height={120}
-                    src={Novotel}
-                alt='taj'/>
-                <Image
-                    width={80}
-                    height={80}
-                    src={itc}
-                alt='taj'/>
-                <Image
-                    width={100}
-                    height={100}
-                    src={indigo}
-                alt='taj'/>
+            <div className='w-[80%] '>
+                <Marquee speed={20}>
+                    <div className=' flex items-center justify-center gap-x-8'>
+                        <Image
+                            className=''
+                            width={80}
+                            height={80}
+                            src={taj}
+                            alt='taj' />
+                        <Image
+                            width={120}
+                            height={120}
+                            src={westin}
+                            alt='taj' />
+                        <Image
+                            width={120}
+                            height={120}
+                            src={hyatt}
+                            alt='taj' />
+                        <Image
+                            width={120}
+                            height={120}
+                            src={Novotel}
+                            alt='taj' />
+                        <Image
+                            width={80}
+                            height={80}
+                            src={itc}
+                            alt='taj' />
+                        <Image
+                            width={100}
+                            height={100}
+                            src={indigo}
+                            alt='taj' />
+                    </div>
+                </Marquee>
             </div>
+           
+           
         </div>
     )
 }
