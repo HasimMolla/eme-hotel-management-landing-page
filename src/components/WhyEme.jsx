bec7d831829352197f334f5a4f9a7825467f34c1
Molla1441 <mhasim1441@gmail.com>
2024-08-10 12:39:56 +0530

    fix: CTA button add in why eme

--- src/components/WhyEme.jsx
+++ src/components/WhyEme.jsx
@@ -1,16 +1,13 @@
 'use client'
-import { useState } from "react";
-import AdmissionModal from "./AdmissionModal";
-import CourseData from '@/data/course.json'
 import CTAButton from "./CTAButton";
 
 export default function WhyEme() {
    
 
     return (
-        <div id='whyEME' className="w-full  max-w-[1380px] mx-auto  py-5 md:py-[70px]  px-[20px] md:px-24 ">
+        <div id='whyEME' className="w-full  max-w-[1380px] mx-auto  py-5 md:py-[70px]  px-[20px] md:px-24 flex flex-col gap-y-10 ">
             <div className="w-full flex flex-col md:flex-row justify-center  gap-[40px]">
-                <div className="w-full md:w-[40%] flex flex-col gap-y-1 md:gap-y-4 justify-center items-center md:justify-start  text-[#042A76] text-3xl lg:leading-10 lg:text-5xl  font-bold  ">
+                <div className="w-full md:w-[40%] flex flex-col  gap-y-1 md:gap-y-4 justify-center items-center md:justify-start  text-[#042A76] text-3xl lg:leading-10 lg:text-5xl  font-bold  ">
                     <div><span className="text-[#E87D1A]">W</span>hy study with  </div>
                     <div className='  text-[#2251FF] lg:leading-10 lg:text-5xl  font-bold '>EME ACADEMY
                         <span><svg className="w-[225px] md:w-[301px]" xmlns="http://www.w3.org/2000/svg" width="301" height="10" viewBox="0 0 301 10" fill="none">
@@ -116,12 +113,12 @@ export default function WhyEme() {
                     </div>
 
                 </div>
-                <div>
-                    <CTAButton/>
-                </div>
+                
 
             </div>
-           
+            <div>
+                <CTAButton />
+            </div>
             
 
         </div>
